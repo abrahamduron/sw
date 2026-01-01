0eac418bd4b153818ebdfb0380c5009a46a2e6b0
Abraham de Jesus Duron Rios <abrahamduron20@gmail.com>
2026-01-01 10:57:26 -0600

    Initial commit from local project

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'SoporteWebs';
+export const SITE_DESCRIPTION = 'Servicios profesionales de desarrollo web, mantenimiento, migración de plataformas, ecommerce y pentesting web.';
